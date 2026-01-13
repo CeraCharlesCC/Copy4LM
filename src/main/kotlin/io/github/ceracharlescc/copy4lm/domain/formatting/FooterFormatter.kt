f9e654e2d247d24ab0f86de2909e75ece7861150
C.C. <36757974+CeraCharlesCC@users.noreply.github.com>
2026-01-14 03:26:32 +0900

    Remove HeaderFormatter and FooterFormatter classes
    Deleted the HeaderFormatter and FooterFormatter utility objects from the formatting domain. These classes are no longer needed or their functionality has been refactored elsewhere.

--- src/main/kotlin/io/github/ceracharlescc/copy4lm/domain/formatting/FooterFormatter.kt
+++
@@ -1,7 +0,0 @@
-package io.github.ceracharlescc.copy4lm.domain.formatting
-
-internal object FooterFormatter {
-    fun format(footerFormat: String, relativePath: String): String {
-        return footerFormat.replace($$"$FILE_PATH", relativePath)
-    }
-}
--- src/main/kotlin/io/github/ceracharlescc/copy4lm/domain/formatting/HeaderFormatter.kt
+++
@@ -1,7 +0,0 @@
-package io.github.ceracharlescc.copy4lm.domain.formatting
-
-internal object HeaderFormatter {
-    fun format(headerFormat: String, relativePath: String): String {
-        return headerFormat.replace($$"$FILE_PATH", relativePath)
-    }
-}
