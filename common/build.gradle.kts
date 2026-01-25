a09e2858703e5cdfd3f3147faa9997717afe372c
C.C. <36757974+CeraCharlesCC@users.noreply.github.com>
2026-01-25 18:16:08 +0900

    Update build.gradle.kts

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -19,7 +19,11 @@ kotlin {
     }
 
     sourceSets {
-        val commonMain by getting
+        val commonMain by getting {
+            dependencies {
+                implementation(kotlin("stdlib"))
+            }
+        }
         val commonTest by getting {
             dependencies {
                 implementation(kotlin("test"))
